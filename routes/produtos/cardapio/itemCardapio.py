b7a62d8871ff59ef1b67f1a125556e69b0bb1222
Gabriel Coutinho <165735404+gabrielcoutinhoDL@users.noreply.github.com>
2026-05-13 15:20:00 -0300

    Finalizando o CRUD (itensCardapio)

--- routes/produtos/cardapio/itemCardapio.py
+++ routes/produtos/cardapio/itemCardapio.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, jsonify
 from database import supabase
-from pip._vendor.urllib3 import response
 
 itemCardapio_bp = Blueprint('itemCardapio', __name__)
 
@@ -25,9 +24,59 @@ def criar_item_cardapio():
         }).execute()
         
         return jsonify({
-            "mensagem": "Item cadastrado com sucesso!"
+            "mensagem": "Item cadastrado com sucesso!",
             "Item": response.data[0] if response.data else None
         }), 201
         
     except Exception as e:
         return jsonify({"erro": f"Erro ao cadastrar item: {str(e)}"}), 500
+    
+@itemCardapio_bp.route ("/itensCardapio", methods=["GET"])
+def listar_item_cardapio():
+    try:
+        response = supabase.table("itens_cardapio").select("*").execute()
+        return jsonify({"itens": response.data}), 200
+    except Exception as e:
+        return jsonify({"erro": f"Erro ao listar itens: {str(e)}"}), 500
+
+@itemCardapio_bp.route ("/itensCardapio/<int:item_id>", methods=["PUT"])
+def atualizar_item_cardapio (item_id):
+    try:
+        dados = request.json
+        if not dados:
+            return jsonify({"erro": "Dados não fornecidos"}), 500
+        
+        update_data = {}
+        
+        campos_permitidos = ["nome", "descricao", "preco", "imagem_url", "categoria"]
+        for campo in campos_permitidos:
+            if campo in dados:
+                update_data[campo] = dados[campo]
+                
+        if not update_data:
+            return jsonify({"erro": "Nenhum campo válido para atualização"}), 400
+                
+        response = supabase.table("itens_cardapio").update(update_data).eq("id", item_id).execute()
+            
+        if not response.data:
+            return jsonify({"erro": "Item não encontrado"}), 404
+            
+        return jsonify({
+            "mensagem": "Item atualizado com sucesso",
+            "item": response.data[0]
+        }), 200
+            
+    except Exception as e:
+        return jsonify({"erro": f"Erro ao atualizar item: {str(e)}"}), 500
+    
+@itemCardapio_bp.route ("/itensCardapio/<int:item_id>", methods=["DELETE"])
+def deletar_item_cardapio (item_id):
+    try:
+        response = supabase.table("itens_cardapio").delete().eq("id", item_id).execute()
+        
+        if not response.data:
+            return jsonify({"erro": "Item não encontrado"}), 404
+        
+        return jsonify({"mensagem": "Item deletado com sucesso"}), 200
+    except Exception as e:
+        return jsonify({"erro": f"Erro ao deletar o item: {str(e)}" }), 500
