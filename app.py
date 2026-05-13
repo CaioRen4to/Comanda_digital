efbb00e642d04a5e186d28567cae37747293d262
CaioRen4to <caio.renato011@gmail.com>
2026-05-12 21:14:02 -0300

    feat: Criação dos QR CODE e separação de uuid de cada mesa, além implementação da pasta templates

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-from supabase import create_client, Client
-from flask import Flask, jsonify, redirect, request
+from flask import Flask, jsonify, redirect, request, render_template
 import os
 from dotenv import load_dotenv
 from routes.produtos.produtos import produtos_bp
 from routes.produtos.cardapio.itemCardapio import itemCardapio_bp
+from routes.mesas.mesas import mesas_bp
 from database import supabase
+
 
 load_dotenv()
 
-# Teste de leitura do .env
+app = Flask(__name__)
+app.register_blueprint(produtos_bp)
+app.register_blueprint(itemCardapio_bp)
+app.register_blueprint(mesas_bp)
+
+@app.route("/")
+def home():
+    return redirect("/mesas")
+
+
 print(f"--- DEBUG ENV ---")
 print(f"SUPABASE_URL: {'Carregado' if os.getenv('SUPABASE_URL') else 'NÃO CARREGADO'}")
 print(f"SUPABASE_KEY: {'Carregado' if os.getenv('SUPABASE_KEY') else 'NÃO CARREGADO'}")
 print(f"-----------------")
 
 
-app = Flask(__name__)
-app.register_blueprint(produtos_bp)
-app.register_blueprint(itemCardapio_bp)
-app.register_blueprint(mesas_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- routes/mesas/mesas.py
+++ routes/mesas/mesas.py
@@ -1,34 +1,29 @@
-from flask import Flask, Blueprint, request, jsonify
-from supabase import create_client, Client
-import os
+from flask import Blueprint, request, jsonify, render_template
+from database import supabase
 import uuid
 
 
-mesas_bp = Blueprint('mesas', __name__)
-def get_supabase():
-    url = os.getenv("SUPABASE_URL")
-    key = os.getenv("SUPABASE_KEY")
-    return create_client(url, key)
+mesas_bp = Blueprint('mesas', __name__, template_folder='../../templates')
 
 
 @mesas_bp.route("/mesas", methods=["POST"])
 def criar_mesa():
-    
-    supabase = get_supabase()
+
     numero = request.json.get("numero")
+
+    if not numero:
+        return jsonify({"erro": "Número da mesa é obrigatório"}), 400
+
     token = str(uuid.uuid4())  #Gerar um token único para a mesa
-    
+
     mesa = supabase.table("mesas").insert({
         "numero": numero,
         "status": "livre",
         "qr_code_token": token
     }).execute()
-    
+
     url_qrcode = f"http://localhost:3000/mesa/{token}"  #URL para acessar a mesa via QR code
-    
-    if not numero:
-        return jsonify({"erro": "Número da mesa é obrigatório"}), 400
-    
+
     return jsonify({
         "mensagem": "Mesa criada com sucesso",
         "id": mesa.data[0]['id'],
@@ -36,10 +31,25 @@ def criar_mesa():
     })
 
 
-
+@mesas_bp.route("/mesa/<token>", methods=["GET"])
+def abrir_cardapio(token):
+    
+    mesa = supabase.table("mesas").select("*").eq("qr_code_token", token).execute()
+    
+    if not mesa.data:
+        return jsonify({"erro": "Mesa não encontrada"}), 404
+    
+    produtos = supabase.table("produtos").select("*").eq('ativo', True).execute()
+    
+    return render_template(
+        "cardapio.html", 
+        mesa=mesa.data[0], 
+        produtos=produtos.data
+    )
+    
+    
 @mesas_bp.route("/mesas", methods=["GET"])
 def listar_mesas():
-    supabase = get_supabase()
     mesas = supabase.table("mesas").select("*").execute()
     return jsonify(mesas.data)
 
@@ -48,7 +58,6 @@ def listar_mesas():
 @mesas_bp.route("/mesas/<int:id>", methods=["PUT"])
 def atualizar_mesa(id):
     status = request.json.get("status")
-    supabase = get_supabase()
     supabase.table("mesas").update({
         "status": status
     }).eq("id", id).execute()
@@ -61,7 +70,6 @@ def atualizar_mesa(id):
 
 @mesas_bp.route("/mesas/<int:id>", methods=["DELETE"])
 def deletar_mesa(id):
-    supabase = get_supabase()
     supabase.table("mesas").delete().eq("id", id).execute()
     return jsonify({
         "mensagem": "Mesa deletada com sucesso"
